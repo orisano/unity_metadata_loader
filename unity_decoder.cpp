842b706e0c607c4e9fee9749eb5b416726ccb209
Nao YONASHIRO <owan.orisano@gmail.com>
2019-12-29 18:55:32 +0900

    style: use clang-format

--- unity_decoder.cpp
+++ unity_decoder.cpp
@@ -1,103 +1,104 @@
-#include <fstream>
-#include <ios>
+#include <algorithm>
 #include <cstdlib>
+#include <fstream>
+#include <il2cpp-metadata.h>
+#include <ios>
 #include <map>
 #include <string>
-#include <algorithm>
-#include <il2cpp-metadata.h>
-#include <vm/String.h>
 #include <vm/MetadataCache.h>
+#include <vm/String.h>
 
-template<typename T>
-T* fromAddr(void *base, size_t offset) {
-    return reinterpret_cast<T*>(static_cast<char*>(base) + offset);
+template <typename T>
+T* fromAddr(void* base, size_t offset) {
+  return reinterpret_cast<T*>(static_cast<char*>(base) + offset);
 }
 
 int main() {
-    using std::ifstream;
-    using std::ofstream;
-    using std::endl;
-    using std::string;
-    using std::map;
-    using std::remove;
-    using std::malloc;
-    using std::free;
-    
-    ifstream metadata_file("./global-metadata.dat", std::ios::binary | std::ios::ate);
+  using std::endl;
+  using std::free;
+  using std::ifstream;
+  using std::malloc;
+  using std::map;
+  using std::ofstream;
+  using std::remove;
+  using std::string;
 
-    size_t metadata_size = metadata_file.tellg();
-    metadata_file.seekg(0, std::ios::beg);
+  ifstream metadata_file("./global-metadata.dat", std::ios::binary | std::ios::ate);
 
-    char* metadata = static_cast<char*>(malloc(metadata_size + 1u));
-    metadata_file.read(metadata, metadata_size);
-    metadata[metadata_size] = 0;
+  size_t metadata_size = metadata_file.tellg();
+  metadata_file.seekg(0, std::ios::beg);
 
-    auto header = reinterpret_cast<Il2CppGlobalMetadataHeader*>(metadata);
-    assert(header->sanity == 0xFAB11BAF);
+  char* metadata = static_cast<char*>(malloc(metadata_size + 1u));
+  metadata_file.read(metadata, metadata_size);
+  metadata[metadata_size] = 0;
 
-    auto usages_lists = fromAddr<Il2CppMetadataUsageList>(metadata, header->metadataUsageListsOffset);
-    auto usage_pairs = fromAddr<Il2CppMetadataUsagePair>(metadata, header->metadataUsagePairsOffset);
-    auto string_literals = fromAddr<Il2CppStringLiteral>(metadata, header->stringLiteralOffset);
-    auto string_literal_data = fromAddr<char>(metadata, header->stringLiteralDataOffset);
+  auto header = reinterpret_cast<Il2CppGlobalMetadataHeader*>(metadata);
+  assert(header->sanity == 0xFAB11BAF);
 
-    map<int, string> plain_strings;
+  auto usages_lists = fromAddr<Il2CppMetadataUsageList>(metadata, header->metadataUsageListsOffset);
+  auto usage_pairs = fromAddr<Il2CppMetadataUsagePair>(metadata, header->metadataUsagePairsOffset);
+  auto string_literals = fromAddr<Il2CppStringLiteral>(metadata, header->stringLiteralOffset);
+  auto string_literal_data = fromAddr<char>(metadata, header->stringLiteralDataOffset);
 
-    auto usage_pairs_size = header->metadataUsageListsCount / sizeof(Il2CppMetadataUsagePair);
-    for (int i = 0; i < usage_pairs_size; i++) {
-        auto usages = usages_lists[i];
-        auto start = usages.start;
-        auto size = usages.count;
+  map<int, string> plain_strings;
 
-        for (int j = 0; j < size; j++) {
-            auto usage_pair = usage_pairs[start + j];
-            auto destination_index = usage_pair.destinationIndex;
-            auto encoded_source_index = usage_pair.encodedSourceIndex;
-            auto usage = GetEncodedIndexType(encoded_source_index);
-            auto decoded_index = GetDecodedMethodIndex(encoded_source_index);
+  auto usage_pairs_size = header->metadataUsageListsCount / sizeof(Il2CppMetadataUsagePair);
+  for (int i = 0; i < usage_pairs_size; i++) {
+    auto usages = usages_lists[i];
+    auto start = usages.start;
+    auto size = usages.count;
 
-            if (usage == kIl2CppMetadataUsageStringLiteral) {
-                auto string_literal = string_literals[decoded_index];
-                plain_strings[destination_index] = string(string_literal_data + string_literal.dataIndex, string_literal.length);
-            }
-        }
+    for (int j = 0; j < size; j++) {
+      auto usage_pair = usage_pairs[start + j];
+      auto destination_index = usage_pair.destinationIndex;
+      auto encoded_source_index = usage_pair.encodedSourceIndex;
+      auto usage = GetEncodedIndexType(encoded_source_index);
+      auto decoded_index = GetDecodedMethodIndex(encoded_source_index);
+
+      if (usage == kIl2CppMetadataUsageStringLiteral) {
+        auto string_literal = string_literals[decoded_index];
+        plain_strings[destination_index] =
+            string(string_literal_data + string_literal.dataIndex, string_literal.length);
+      }
     }
-    auto type_definitions = fromAddr<Il2CppTypeDefinition>(metadata, header->typeDefinitionsOffset);
-    auto string_base = fromAddr<char>(metadata, header->stringOffset);
-    auto methods = fromAddr<Il2CppMethodDefinition>(metadata, header->methodsOffset);
+  }
+  auto type_definitions = fromAddr<Il2CppTypeDefinition>(metadata, header->typeDefinitionsOffset);
+  auto string_base = fromAddr<char>(metadata, header->stringOffset);
+  auto methods = fromAddr<Il2CppMethodDefinition>(metadata, header->methodsOffset);
 
-    map<int, string> method_names;
+  map<int, string> method_names;
 
-    auto type_size = header->typeDefinitionsCount / sizeof(Il2CppTypeDefinition);
-    for (int i = 0; i < type_size; i++) {
-        auto type_definition = type_definitions[i];
-        auto type_name = string(string_base + type_definition.nameIndex);
+  auto type_size = header->typeDefinitionsCount / sizeof(Il2CppTypeDefinition);
+  for (int i = 0; i < type_size; i++) {
+    auto type_definition = type_definitions[i];
+    auto type_name = string(string_base + type_definition.nameIndex);
 
-        auto start = type_definition.methodStart;
-        auto size = type_definition.method_count;
-        for (int j = 0; j < size; j++) {
-            auto method = methods[start + j];
-            auto method_name = string(string_base + method.nameIndex);
-            method_names[method.methodIndex] = type_name + "$$" + method_name;
-        }
+    auto start = type_definition.methodStart;
+    auto size = type_definition.method_count;
+    for (int j = 0; j < size; j++) {
+      auto method = methods[start + j];
+      auto method_name = string(string_base + method.nameIndex);
+      method_names[method.methodIndex] = type_name + "$$" + method_name;
     }
+  }
 
-    {
-        ofstream ofs("./string_literal.txt");
-        ofs << plain_strings.size() << endl;
-        for (auto it = plain_strings.rbegin(); it != plain_strings.rend(); ++it) {
-            auto sl = it->second;
-            sl.erase(remove(sl.begin(), sl.end(), '\r'), sl.end());
-            sl.erase(remove(sl.begin(), sl.end(), '\n'), sl.end());
-            ofs << sl << endl;
-        }
+  {
+    ofstream ofs("./string_literal.txt");
+    ofs << plain_strings.size() << endl;
+    for (auto it = plain_strings.rbegin(); it != plain_strings.rend(); ++it) {
+      auto sl = it->second;
+      sl.erase(remove(sl.begin(), sl.end(), '\r'), sl.end());
+      sl.erase(remove(sl.begin(), sl.end(), '\n'), sl.end());
+      ofs << sl << endl;
     }
+  }
 
-    {
-        ofstream ofs("./method_name.txt");
-        ofs << method_names.size() << endl;
-        for (auto it = method_names.rbegin(); it != method_names.rend(); ++it) {
-            ofs << it->second << endl;
-        }
+  {
+    ofstream ofs("./method_name.txt");
+    ofs << method_names.size() << endl;
+    for (auto it = method_names.rbegin(); it != method_names.rend(); ++it) {
+      ofs << it->second << endl;
     }
-    free(metadata);
+  }
+  free(metadata);
 }
--- unity_inspector.cpp
+++ unity_inspector.cpp
@@ -1,74 +1,76 @@
-#include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include <string>
 #include <il2cpp-metadata.h>
-#include <vm/String.h>
+#include <iostream>
+#include <string>
 #include <vm/MetadataCache.h>
+#include <vm/String.h>
 
-template<typename T>
-T* fromAddr(void *base, size_t offset) {
-    return reinterpret_cast<T*>(static_cast<char*>(base) + offset);
+template <typename T>
+T* fromAddr(void* base, size_t offset) {
+  return reinterpret_cast<T*>(static_cast<char*>(base) + offset);
 }
 
-int main(int argc, char **argv) {
-    using std::cerr;
-    using std::cout;
-    using std::hex;
-    using std::endl;
-    using std::atoi;
-    using std::ifstream;
-    using std::string;
-    using std::malloc;
-    using std::free;
-    
-    if (argc == 1) {
-        cerr << "too few arguments" << endl;
-        cerr << "Usage: inspector [type_index]" << endl;
-        return 1;
+int main(int argc, char** argv) {
+  using std::atoi;
+  using std::cerr;
+  using std::cout;
+  using std::endl;
+  using std::free;
+  using std::hex;
+  using std::ifstream;
+  using std::malloc;
+  using std::string;
+
+  if (argc == 1) {
+    cerr << "too few arguments" << endl;
+    cerr << "Usage: inspector [type_index]" << endl;
+    return 1;
+  }
+  int type_index = atoi(argv[1]);
+
+  ifstream metadata_file("./global-metadata.dat", std::ios::binary | std::ios::ate);
+
+  size_t metadata_size = metadata_file.tellg();
+  metadata_file.seekg(0, std::ios::beg);
+
+  char* metadata = static_cast<char*>(malloc(metadata_size + 1u));
+  metadata_file.read(metadata, metadata_size);
+  metadata[metadata_size] = 0;
+
+  auto header = reinterpret_cast<Il2CppGlobalMetadataHeader*>(metadata);
+  assert(header->sanity == 0xFAB11BAF);
+
+  auto type_defs = fromAddr<Il2CppTypeDefinition>(metadata, header->typeDefinitionsOffset);
+  auto type_def = type_defs[type_index];
+
+  auto fields = fromAddr<Il2CppFieldDefinition>(metadata, header->fieldsOffset);
+
+  auto names = fromAddr<char>(metadata, header->stringOffset);
+
+  auto field_default_values =
+      fromAddr<Il2CppFieldDefaultValue>(metadata, header->fieldDefaultValuesOffset);
+  auto field_default_values_size =
+      header->fieldDefaultValuesCount / sizeof(Il2CppFieldDefaultValue);
+
+  cout << string(names + type_def.nameIndex) << ":" << endl;
+  for (int i = 0; i < type_def.field_count; i++) {
+    int idx = type_def.fieldStart + i;
+    auto field = fields[idx];
+    cout << "\t" << string(names + field.nameIndex) << " = ";
+    for (int j = 0; j < field_default_values_size; j++) {
+      auto fdv = field_default_values[j];
+      if (fdv.fieldIndex == idx) {
+        int addr = header->fieldAndParameterDefaultValueDataOffset + fdv.dataIndex;
+        cout << *fromAddr<int>(metadata, addr);
+        cout << "; // offset: " << hex << addr;
+        goto FOUND;
+      }
     }
-    int type_index = atoi(argv[1]);
+    cout << "?";
+  FOUND:;
+    cout << endl;
+  }
 
-    ifstream metadata_file("./global-metadata.dat", std::ios::binary | std::ios::ate);
-
-    size_t metadata_size = metadata_file.tellg();
-    metadata_file.seekg(0, std::ios::beg);
-
-    char* metadata = static_cast<char*>(malloc(metadata_size + 1u));
-    metadata_file.read(metadata, metadata_size);
-    metadata[metadata_size] = 0;
-
-    auto header = reinterpret_cast<Il2CppGlobalMetadataHeader*>(metadata);
-    assert(header->sanity == 0xFAB11BAF);
-
-    auto type_defs = fromAddr<Il2CppTypeDefinition>(metadata, header->typeDefinitionsOffset);
-    auto type_def = type_defs[type_index];
-
-    auto fields = fromAddr<Il2CppFieldDefinition>(metadata, header->fieldsOffset);
-
-    auto names = fromAddr<char>(metadata, header->stringOffset);
-
-    auto field_default_values = fromAddr<Il2CppFieldDefaultValue>(metadata, header->fieldDefaultValuesOffset);
-    auto field_default_values_size = header->fieldDefaultValuesCount / sizeof(Il2CppFieldDefaultValue);
-
-    cout << string(names + type_def.nameIndex) << ":" << endl;
-    for (int i = 0; i < type_def.field_count; i++) {
-        int idx = type_def.fieldStart + i;
-        auto field = fields[idx];
-        cout << "\t" << string(names + field.nameIndex) << " = ";
-        for (int j = 0; j < field_default_values_size; j++) {
-            auto fdv = field_default_values[j];
-            if (fdv.fieldIndex == idx) {
-                int addr = header->fieldAndParameterDefaultValueDataOffset + fdv.dataIndex;
-                cout << *fromAddr<int>(metadata, addr);
-                cout << "; // offset: " << hex << addr;
-                goto FOUND;
-            }
-        }
-        cout << "?";
-FOUND:;
-      cout << endl;
-    }
-
-    free(metadata);
+  free(metadata);
 }
